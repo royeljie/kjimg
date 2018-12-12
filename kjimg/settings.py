dbeda4d085df3f0c3854d460ef14e86a04c12c7b
Royel.Jie <royel.jie@qq.com>
2018-12-12 11:06:39 +0800

    修改

--- kjimg/settings.py
+++ kjimg/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'whj-agv-4fctm70ge@qclc-ff!km9xk7jn%=^zeb-)elnri=o5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '11.32.81.36']
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
